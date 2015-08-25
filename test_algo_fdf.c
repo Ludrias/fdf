b115938830043ffea316c384ea2f672237ed48a3
Ludrias <quentindoose@gmail.com>
2015-08-25 16:16:14 +0200

    bresenham
    Ajout de l'algo de bresenham

--- test_algo_fdf.c
+++ test_algo_fdf.c
@@ -1,23 +1,57 @@
-/* Algo de tracage de segments */
+/* Algo de tracage de segments 
 
-void	tracage_segment (coordonne_x_gauche, coordonne_y_gauche, coordonne_x_droite, coordonne_y_droite)
+		Merci : http://morpheo.inrialpes.fr/people/Boyer/Teaching/RICM/c2.pdf */
+
+void	tracage_segment_simple (coordonne_x_gauche, coordonne_y_gauche, coordonne_x_droite, coordonne_y_droite)
 {
 	int 	coordonne_x;
 	int		coordonne_y;
 	int		coordonne_x_millieu;
 	int		coordonne_y_millieu;
-	int		mediane;
+	int		moyenne_coordonne;
 
 	coordonne_x = coordonne_x_gauche;
 	coordonne_y = coordonne_y_gauche;
 	coordonne_x_millieu = coordonne_x_droite - coordonne_x_gauche;
 	coordonne_y_millieu = coordonne_y_droite - coordonne_y_gauche;
-	mediane = coordonne_y_millieu / coordonne_x_millieu;
+	moyenne_coordonne = coordonne_y_millieu / coordonne_x_millieu;
 
 	while (coordonne_x <= coordonne_x_droite)
 	{
 		mlx_pixel_put(mlx, win, coordonne_x, coordonne_y , 0xFF0000);
-		coordonne_y += mediane;
+		coordonne_y += moyenne_coordonne;
+		coordonne_x++;
+	}
+}
+
+void	tracage_segment_Bresenham_entier(coordonne_x_gauche, coordonne_y_gauche, coordonne_x_droite, coordonne_y_droite)
+{
+	int 	coordonne_x;
+	int		coordonne_y;
+	int		coordonne_x_millieu;
+	int		coordonne_y_millieu;
+	int 	incrementation_est;
+	int 	incrementation_nordest;
+	int 	moyenne_coordonne_double;
+
+	coordonne_x = coordonne_x_gauche;
+	coordonne_y = coordonne_y_gauche;
+	coordonne_x_millieu = coordonne_x_droite - coordonne_x_gauche;
+	coordonne_y_millieu = coordonne_y_droite - coordonne_y_gauche;
+	incrementation_est = 2 * coordonne_y_millieu;
+	incrementation_nordest = 2* (coordonne_y_millieu - coordonne_x_millieu);
+	moyenne_coordonne_double = 2 * coordonne_y_millieu / coordonne_x_millieu;
+
+	while (coordonne_x <= coordonne_x_droite)
+	{
+		mlx_pixel_put(mlx, win, coordonne_x, coordonne_y , 0xFF0000);
+		if (moyenne_coordonne_double >= 0)
+		{
+			coordonne_y += 1;
+			moyenne_coordonne_double += incrementation_nordest;
+		}
+		else
+			moyenne_coordonne_double += incrementation_est;
 		coordonne_x++;
 	}
 }
